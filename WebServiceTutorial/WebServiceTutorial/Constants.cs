782b473e6279f795f31a9b24cfb0650c0671049c
saif <laamerisaif101@gmail.com>
2022-12-04 17:05:41 +0100

    tp4 api weather

---
+++ WebServiceTutorial/WebServiceTutorial/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace WebServiceTutorial
+{
+    public static class Constants
+    {
+        public const string OpenWeatherMapEndpoint =
+       "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey =
+       "daf8600a3a5336fb83f354807e320f84"; 
+    }
+}
